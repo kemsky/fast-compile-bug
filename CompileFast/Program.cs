7a4fc43ac9d0a0b419fc32a938d8db8e1214daee
Alexander Turtsevich <alexander.turtsevich@itmint.ca>
2023-01-25 01:12:35 +0300

    cleanup

--- CompileFast/Program.cs
+++ CompileFast/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Linq.Expressions;
-using CompileFast.Identity;
 using FastExpressionCompiler;
 
 Expression<Func<ActionItem, bool>> Predicate(Id<AccountManager>? value)
@@ -20,4 +19,16 @@ public class AccountManager
 public class ActionItem
 {
     public long? AccountManagerId { get; set; }
+}
+
+public readonly struct Id<T>
+{
+    private readonly long _value;
+
+    public Id(long value)
+    {
+        _value = value;
+    }
+
+    public static implicit operator long?(Id<T> d) => d._value;
 }
